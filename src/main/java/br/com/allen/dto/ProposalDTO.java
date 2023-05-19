b75ae9238b5525aecd0fc1f2b52963d11b0985b9
AllenLVieira <allenvieira96@gmail.com>
2023-05-18 22:20:55 -0300

    feat: add dtos and repository

---
+++ src/main/java/br/com/allen/dto/ProposalDTO.java
@@ -0,0 +1,19 @@
+package br.com.allen.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+import java.math.BigDecimal;
+
+@Jacksonized
+@Data
+@AllArgsConstructor
+@Builder
+public class ProposalDTO {
+
+    private Long proposalId;
+    private String customer;
+    private BigDecimal priceTonne;
+}
